34e8119a623d7af8d3d73d358420bc51c88626a4
Taylor Hodge <j.taylor.hodge@gmail.com>
2018-12-06 20:59:09 -0500

    complete challenge 6

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -1,8 +1,17 @@
-
 /**
  * Implement a generator function that can be used
  * to generate numbers in the Fibonacci Sequence
  */
-// export function* getFibSequence(): IterableIterator<number> {
- 
-// }
+
+export function* getFibSequence(): IterableIterator<number> {
+  let twoAgo = 1; // 0   1
+  let oneAgo = 0; // 1   1
+  while (true) {
+    let nextValue = twoAgo + oneAgo;
+    yield nextValue;
+    twoAgo = oneAgo;
+    oneAgo = nextValue;
+  }
+}
+
+// 1 1 2
